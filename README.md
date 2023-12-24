2c498696dc12857e25c088204fe728fc08e242dd
Joseph Garcia <prosif@gmail.com>
2023-12-23 21:50:06 -0700

    cool

---
+++ README.md
@@ -0,0 +1,5 @@
+Blog from local files.
+
+`node index.js input_dir output_dir`
+
+eg. `node index.js test_content test_output`
---
+++ index.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+const process = require('process');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
+const args = process.argv;
+
+const filenameIndex = args.findIndex(a => a.endsWith('index.js'));
+
+if (filenameIndex == -1) {
+    console.error("I don't know how this happens. Submit an issue at github.com/prosif/glob");
+    console.error(args);
+    process.exit(1);
+}
+
+if (args.length < filenameIndex + 2) {
+    console.error("Invalid params. Input and output directory required");
+    process.exit(1);
+}
+
+const inDir = args[filenameIndex + 1];
+const outDir = args[filenameIndex + 2];
+
+fs.readdir(inDir, (err, entries) => {
+    const times = new Array();
+    const entryMap = {};
+
+    entries.forEach(entry => {
+        const contentPath = path.join(inDir, path.join(entry, 'content.txt'));
+        const imageBasePath = path.join(inDir, path.join(entry, 'image'));
+        const hasJpg = fs.existsSync(imageBasePath + '.jpg'); 
+        const hasPng = fs.existsSync(imageBasePath + '.png'); 
+        const hasSvg = fs.existsSync(imageBasePath + '.svg'); 
+
+        const hasContent = fs.existsSync(contentPath);
+
+        if (hasContent) {
+            const info = fs.statSync(contentPath);
+            times.push(info.birthtimeMs);
+
+            const hasImage = hasJpg || hasPng || hasSvg;
+            let imagePath = null;
+            if (hasJpg) {
+                imagePath = imageBasePath + '.jpg';
+            }
+    
+            if (hasPng) {
+                if (hasJpg) {
+                    console.warn("Multiple images found for " + entry + ". Ignoring png");
+                } else {
+                    imagePath = imageBasePath + '.png';
+                }
+            }
+    
+            if (hasSvg) {
+                if (hasJpg || hasPng) {
+                    console.warn("Multiple images found for " + entry + ". Ignoring svg");
+                } else {
+                    imagePath = imageBasePath + '.svg';
+                }
+            }
+
+            entryMap['' + info.birthtimeMs] = {entry, info, imagePath, contentPath};
+        } else {
+            console.warn('Unable to find content at ' + contentPath);
+        }
+    });
+
+    const sortedEntries = new Array();
+    times.sort().forEach(time => {
+        const entry = entryMap['' + time];
+        sortedEntries.push(entry);
+    });
+
+    writeOutput(sortedEntries, outDir);
+});
+
+
+const writeOutput = (entries, outDir) => {
+    if (!fs.existsSync(outDir)) {
+        fs.mkdirSync(outDir);
+    }
+
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
+        const title = entry.entry;
+
+        const header = `<h1>${entry.entry}</h1>`;
+        const mainContent = fs.readFileSync(entry.contentPath).toString().replaceAll(os.EOL, `</p><p>`);
+        const content = `<p>${mainContent}</p>`;
+        let imageDiv = '';
+        if (entry.imagePath) {
+            // lol this sucks
+            const fileExtension = entry.imagePath.split('.')[entry.imagePath.split('.').length - 1]; 
+            fs.copyFileSync(entry.imagePath, path.join(outDir, `${i + 1}.${fileExtension}`));
+            imageDiv = `<img src="/${i + 1}.${fileExtension}"></img>`;
+        }
+
+        const prevLink = i === 0 ? '' : `<a href="/${i}.html">${i}</a>`;
+        const nextLink = i === entries.length - 1 ? '' : `<a href="/${i + 2}.html">${i + 2}</a>`;
+
+        const createdText = `Created ${new Date(entry.info.birthtimeMs).toUTCString()}`;
+        const updatedText = `Last updated ${new Date(entry.info.mtimeMs).toUTCString()}`;
+
+        const meta = `<div>${createdText}${updatedText}</div>`;
+
+        const links = `<div>${prevLink}${nextLink}</div>`;
+
+        const outputHtml = `<!DOCTYPE HTML><html><head><link rel="stylesheet" href="style.css"><title>${title}</title></head><body>${links}${header}${imageDiv}${meta}${content}</body></html>`;
+        fs.writeFileSync(path.join(outDir, '' + (i + 1) + '.html'), outputHtml);
+
+        if (i === entries.length - 1) {
+            fs.writeFileSync(path.join(outDir, 'index.html'), outputHtml);
+        }
+    };
+    
+    fs.copyFileSync(path.join(__dirname, 'style.css'), path.join(outDir, 'style.css'));
+}
